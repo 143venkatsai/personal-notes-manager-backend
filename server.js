38f41b7b0e8da8909c839c8cf8405896d69f8627
143venkatsai <venkatsaipelluru143@gamil.com>
2024-11-23 07:15:17 +0530

    updated db

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const notesRoutes = require("./routes/notes");
 const app = express();
 
 const PORT = process.env.PORT || 5001
-const MONGODB_URL = process.env.MONGO_URL
+const {DB_USER, DB_PASSWORD} = process.env
+
+const MONGODB_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@notes.lqt9g.mongodb.net/notes?retryWrites=true&w=majority&appName=notes`
+
 
 app.use(express());
 app.use(bodyParser.json());
@@ -21,7 +24,7 @@ mongoose.connect(MONGODB_URL, {useNewUrlParser: true,useUnifiedTopology: true})
 .then(() =>{
     console.log("Connected to MongoDB");
     app.listen(PORT, () =>{
-        console.log(`Server is running on port ${PORT}`)
+        console.log(`Server is running on port ${PORT}`);
     });
 })
 .catch((err) => console.log(err))
